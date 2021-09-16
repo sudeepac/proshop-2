ce289f08cf00a57f464db2423bc9fe95d15cd820
sudeepac <sudeepac@gmail.com>
2021-09-16 18:31:48 +1000

    Serving and fetching data from Express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { HomeScreen } from "./screens/HomeScreen";
+import { ProductScreen } from "./screens/ProductScreen";
 
 export const App = () => {
   return (
-    <div>
-      <h1>Welcome to proshop</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 };
